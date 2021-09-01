4034a11173dd1d524a5d0256eca6b96c82bc4814
Kirill Stroganov <myp2zz@yandex.ru>
2021-09-01 17:38:53 +0300

    Add test case for health

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -31,53 +31,52 @@ func (h *Handler) SetupRouters() {
 	h.Router.HandleFunc("/api/stat/", h.GetAllStats).Methods(http.MethodGet)
 	h.Router.HandleFunc("/api/stat/", h.PostStat).Methods(http.MethodPost)
 
-	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "alive!")
-		if err != nil {
-			return
-		}
-	})
+	h.Router.HandleFunc("/api/health/", h.CheckHealth).Methods(http.MethodGet)
 }
 
+//GetStats - ...
 func (h *Handler) GetStats(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
 	i, err := strconv.ParseUint(id, 10, 64)
 	if err != nil {
-		_, err := fmt.Fprintf(w, "Unable to parse uint from id")
-		if err != nil {
-			return
-		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		http.Error(w, "Unable to parse uint from id", http.StatusBadRequest)
+		return
 	}
+
 	stat, err := h.Service.GetStats(uint(i))
 	if err != nil {
-		_, err := fmt.Fprintf(w, "Can't get stat by this ID")
-		if err != nil {
-			return
-		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		http.Error(w, "Can't get stat by this ID", http.StatusBadRequest)
+		return
 	}
+
 	_, err = fmt.Fprintf(w, "%+v", stat)
 	if err != nil {
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 }
 
+//GetAllStats - ...
 func (h Handler) GetAllStats(w http.ResponseWriter, r *http.Request) {
 	allStats, err := h.Service.GetAllStats()
 	if err != nil {
-		_, err := fmt.Fprintf(w, "Can't get all stats")
-		if err != nil {
-			return
-		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		http.Error(w, "Can't get all stats", http.StatusInternalServerError)
+		return
 	}
 
 	_, err = fmt.Fprintf(w, "%+v", allStats)
 	if err != nil {
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 }
 
+//PostStat - ...
 func (h Handler) PostStat(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -94,10 +93,9 @@ func (h Handler) PostStat(w http.ResponseWriter, r *http.Request) {
 	}
 	i, err := strconv.ParseUint(id, 10, 64)
 	if err != nil {
-		_, err := fmt.Fprintf(w, "Unable to parse uint from id")
-		if err != nil {
-			return
-		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		http.Error(w, "Unable to parse uint from id", http.StatusBadRequest)
+		return
 	}
 
 	statsType := r.Form.Get("type")
@@ -117,10 +115,18 @@ func (h Handler) PostStat(w http.ResponseWriter, r *http.Request) {
 	newStat := storage.Stats{StatsType: statsType, StatsValue: statsValue}
 	err = h.Service.PostStats(uint(i), newStat)
 	if err != nil {
-		_, err := fmt.Fprintf(w, "Can't save stat")
-		if err != nil {
-			return
-		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		http.Error(w, "Can't save stat", http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+// CheckHealth - endpoint to check health
+func (h Handler) CheckHealth(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "alive!")
+	if err != nil {
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
---
+++ internal/transport/http/handler_test.go
@@ -0,0 +1,113 @@
+package http
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/kiraplenkin/go-musthave-devops/internal/stats"
+	"github.com/kiraplenkin/go-musthave-devops/internal/storage"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetStats(t *testing.T) {
+	type fields struct {
+		Router  *mux.Router
+		Service *stats.Service
+	}
+	type want struct {
+		code        int
+		contentType string
+		text        string
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		endpoint string
+		want     want
+	}{
+		{
+			name: "test",
+			fields: fields{
+				Router:  mux.NewRouter(),
+				Service: stats.NewService(storage.New()),
+			},
+			endpoint: "/api/stat/1",
+			want: want{
+				code:        http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+				text:        "This Id doesn''t exists\n",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler := &Handler{
+				Router:  tt.fields.Router,
+				Service: tt.fields.Service,
+			}
+			req := httptest.NewRequest(http.MethodGet, tt.endpoint, nil)
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(handler.GetStats)
+			h.ServeHTTP(w, req)
+			res := w.Result()
+			assert.Equal(t, tt.want.code, res.StatusCode)
+			assert.Equal(t, tt.want.contentType, res.Header.Get("Content-Type"))
+			b, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Errorf(err.Error())
+			}
+			assert.Equal(t, tt.want.text, string(b))
+		})
+	}
+}
+
+func TestHandler_CheckHealth(t *testing.T) {
+	type fields struct {
+		Router  *mux.Router
+		Service *stats.Service
+	}
+	type want struct {
+		code int
+		text string
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		endpoint string
+		want     want
+	}{
+		{
+			name: "Check health",
+			fields: fields{
+				Router:  mux.NewRouter(),
+				Service: stats.NewService(storage.New()),
+			},
+			endpoint: "/api/health/",
+			want: want{
+				code: http.StatusOK,
+				text: "alive!",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler := Handler{
+				Router:  tt.fields.Router,
+				Service: tt.fields.Service,
+			}
+			req := httptest.NewRequest(http.MethodGet, tt.endpoint, nil)
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(handler.CheckHealth)
+			h.ServeHTTP(w, req)
+			res := w.Result()
+			assert.Equal(t, tt.want.code, res.StatusCode)
+			b, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Errorf(err.Error())
+			}
+			assert.Equal(t, tt.want.text, string(b))
+		})
+	}
+}
