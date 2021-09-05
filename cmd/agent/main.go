808d0aa74d90dccfc26f21e06d3fa20bed575cf0
Stroganov Kirill <myp2zz@yandex.ru>
2021-09-05 22:02:41 +0300

    Fix errors

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/kiraplenkin/go-musthave-devops/internal/sender"
+	sendingService "github.com/kiraplenkin/go-musthave-devops/internal/sender"
 	"github.com/kiraplenkin/go-musthave-devops/internal/types"
 	"time"
 )
 
 func main() {
-	sender := sender.NewSender()
-	logger := sender.NewLogger(*sender)
+	sender := sendingService.NewSender()
+	logger := sendingService.NewLogger(*sender)
 
 	for {
 		ticker := time.NewTicker(types.SenderConfig.ServerUpdateTime)
