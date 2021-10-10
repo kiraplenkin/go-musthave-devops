46a8eb87e4480d43f163426d0b4df3b2b81cbb1b
kiraplenkin <myp2zz@yandex.ru>
2021-10-10 17:02:51 +0300

    Test

--- internal/types/types.go
+++ internal/types/types.go
@@ -43,7 +43,7 @@ type (
 
 	// Storage struct of storage
 	Storage struct {
-		Mu             *sync.Mutex
+		Mu             sync.Mutex
 		GaugeStorage   map[string]Stats
 		CounterStorage map[string]int64
 	}
