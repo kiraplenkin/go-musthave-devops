418b8c2061b832e260a4fed2860ddd3faa7b5b2e
Kirill <kirr@MacBook-Pro-Kirill.local>
2021-10-14 16:23:08 +0300

    test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -74,7 +74,7 @@ func main() {
 	}()
 
 	<-done
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	err = store.WriteToFile()
 	if err != nil {
